117d7759eafecd87e9e184ef53223337b9925053
Senh Mo Chuang <senhmo@gmail.com>
2021-09-19 09:17:07 -0500

    night lights

--- sunset.py
+++ sunset.py
@@ -3,7 +3,7 @@
 import subprocess
 from time import sleep
 from Confidential.switch_table import switch1on
-# from Confidential.switch_table import switch3off
+from Confidential.switch_table import switch3off
 
 
 def running_from_cli(command):
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     running_from_cli(f"/var/www/rfoutlet/codesend {switch1on}")
     sleep(1)
     running_from_cli(f"/var/www/rfoutlet/codesend {switch1on}")
-    # sleep(1)
-    # running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
-    # sleep(1)
-    # running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
-    # sleep(1)
-    # running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
+    sleep(1)
+    running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
+    sleep(1)
+    running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
+    sleep(1)
+    running_from_cli(f"/var/www/rfoutlet/codesend {switch3off}")
