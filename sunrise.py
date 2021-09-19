37997837d415f5e169696f09ea67106fb55cad87
Senh Mo Chuang <senhmo@gmail.com>
2021-09-19 10:43:30 -0500

    Added Night Light

--- sunrise.py
+++ sunrise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from base_action import run_event
 from Confidential.switch_table import switch1off
-
-run_event([switch1off])
+from Confidential.switch_table import switch2off
+run_event([switch1off, switch2off])
--- sunset.py
+++ sunset.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from base_action import run_event
 from Confidential.switch_table import switch1on
+from Confidential.switch_table import switch2on
 
-run_event([switch1on])
+run_event([switch1on, switch2on])
